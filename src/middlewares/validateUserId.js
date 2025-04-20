27d62c72e67b1d5c2359b792454ddd13b0c5512f
Sovitou Khem <khemsovitou@gmail.com>
2025-04-20 22:51:07 +0700

    Fix middleware function signature to include next parameter in validateUserId

--- src/middlewares/validateUserId.js
+++ src/middlewares/validateUserId.js
@@ -8,11 +8,12 @@ export const validateUserId = [
     .isNumeric()
     .withMessage("ID must be a number"),
 
-  (req, res) => {
+  (req, res, next) => {
     const error = validationResult(req);
     if (!error.isEmpty()) {
       return res.status(400).json({ error: error.array() });
     }
+    next();
   },
 
   async (req, res, next) => {
