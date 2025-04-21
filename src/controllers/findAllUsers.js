347a7a182cca2bfd35e853c22ac32e54c9e8630d
Sovitou Khem <khemsovitou@gmail.com>
2025-04-21 11:10:16 +0700

    Add global error handling middleware and uncomment cookie setting in findAllUsers

--- src/controllers/findAllUsers.js
+++ src/controllers/findAllUsers.js
@@ -2,6 +2,8 @@ import { USERS } from "../models/userDB.js";
 
 export const findAllUsers = async (req, res) => {
   try {
+    // res.cookie("Cookies", "User login", { maxAge: 60000 });
+
     const getAllUsers = await USERS.findAll();
     res.status(200).json(getAllUsers);
   } catch (error) {
